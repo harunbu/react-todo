9e6424d859916f109f79f79170b09601fb36750c
HarunobuTsutsumi <h.tsutsumi1203@gmail.com>
2020-05-13 22:19:45 +0900

    todoアプリのひな型作成

---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export function MenuBar(props) {
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      <AppBar position="fixed" color="primary">
+        <Container maxWidth="sm">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Todo リスト
+          </Typography>
+          {/*<Button color="inherit">Login</Button>*/}
+        </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import { MenuBar } from './components/MenuBar.jsx'
+import { Toolbar } from '@material-ui/core';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function InputForm(props) {
+  return (
+    <Card elevation={3}>
+      <CardContent>
+        <Box display="flex" alignItems="center">
+          <Box gbcolor="gray.300" flexGrow={1}>
+            <TextField fullWidth id="standard-basic" label="タスクの内容を入力" />
+          </Box>
+          <Button variant="contained">タスク追加</Button>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  handleButtonClick() {
+    alert('click!');
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        {/* メニューバー */}
+        <MenuBar />
+        <Toolbar />
+
+        {/* ToDoList */}
+        <Container maxWidth="sm">
+          <Box p={2}>
+            <InputForm />
+          </Box>
+        </Container>
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
